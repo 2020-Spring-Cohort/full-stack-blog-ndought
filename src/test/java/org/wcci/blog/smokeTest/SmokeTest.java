1d2ed2109e975b16d9a638ebb27c99b0effaec7b
Noah Doughty <noahdoughty@gmail.com>
2020-02-23 15:13:09 -0500

    SmokeTest
    SmokeTest ran and passed.

---
+++ src/test/java/org/wcci/blog/smokeTest/SmokeTest.java
@@ -0,0 +1,15 @@
+package org.wcci.blog.smokeTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DirtiesContext
+@SpringBootTest
+class SmokeTest {
+
+    @Test
+    void contextLoads() {
+        
+    }
+}
