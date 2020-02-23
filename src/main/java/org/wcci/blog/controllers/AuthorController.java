3f9660e8928b0e73847060d438f047763a51bae9
Noah Doughty <noahdoughty@gmail.com>
2020-02-22 21:21:25 -0500

    Second Commit
    Created Author POJO

--- src/main/java/org/wcci/blog/controllers/AuthorController.java
+++ src/main/java/org/wcci/blog/controllers/AuthorController.java
@@ -1,4 +1,10 @@
 package org.wcci.blog.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/authors")
 public class AuthorController {
 }
--- src/main/java/org/wcci/blog/models/Author.java
+++ src/main/java/org/wcci/blog/models/Author.java
@@ -1,4 +1,51 @@
 package org.wcci.blog.models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Collection;
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String author;
+
+    @ManyToMany(mappedBy = "authors")
+    private Collection<Post> posts;
+
+    public Author() {}
+
+    public Author(String author) {
+        this.author = author;}
+
+    public Collection<Post> getPosts() {
+             return posts;}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void addPostsToAuthor(Collection<Post> posts) {
+        this.posts = posts;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", posts=" + posts +
+                '}';
+    }
 }
+
+
