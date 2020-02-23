dd4f97a1486d480342d43660eca436d978eb0e0f
Noah Doughty <noahdoughty@gmail.com>
2020-02-23 18:17:18 -0500

    CategoryControllerTest
    CategoryControllerTest created with 4/4 tests passing, CategoryController created with one error in CategoryController method 'storage' - not able to autowire because no beans of CategoryStorage found????

--- src/main/java/org/wcci/blog/storage/CategoryStorage.java
+++ src/main/java/org/wcci/blog/storage/CategoryStorage.java
@@ -1,4 +1,13 @@
 package org.wcci.blog.storage;
 
+import org.wcci.blog.models.Category;
+
 public interface CategoryStorage {
+
+
+    Object findCategoryByName(String tech);
+
+    Object getAll();
+
+    void store(Category category);
 }
--- src/test/java/org/wcci/blog/controllerTest/CategoryControllerTest.java
+++ src/test/java/org/wcci/blog/controllerTest/CategoryControllerTest.java
@@ -1,11 +1,86 @@
 package org.wcci.blog.controllerTest;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoProperties;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
 import org.wcci.blog.controllers.CategoryController;
+import org.wcci.blog.models.Category;
+import org.wcci.blog.storage.CategoryStorage;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class CategoryControllerTest {
 
     private MockMvc mockMvc;
     private CategoryController underTest;
-    private CategoryStorage mockStorage
+    private CategoryStorage mockStorage;
+    private Model mockModel;
+
+    @Autowired
+    private CategoryStorage storage;
+
+    @BeforeEach
+    void setUp() {
+        mockModel = mock(Model.class);
+        mockStorage = mock(CategoryStorage.class);
+        underTest = new CategoryController(mockStorage);
+        mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
+    }
+    @Test
+    public void shouldReturnViewWithOneCategory() {
+        Category testCategory = new Category("tech");
+        when(mockStorage.findCategoryByName("tech")).thenReturn(testCategory);
+
+        underTest.displaySingleCategory("tech", mockModel);
+
+        verify(mockStorage).findCategoryByName("tech");
+        verify(mockModel).addAttribute("category", testCategory);
+    }
+    @Test
+    public void shouldReturnViewNamedCategoryWhenDisplaySingleCategoryIsCalled() {
+        String viewName = underTest.displaySingleCategory("tech", mockModel);
+        assertThat(viewName).isEqualTo("category");
+    }
+    @Test
+    public void shouldGoToIndividualEndPoint() throws Exception {
+        Category testCategory = new Category("sports");
+        when(mockStorage.findCategoryByName("sports")).thenReturn(testCategory);
+
+        mockMvc.perform(get("/category/sports"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("category"))
+                .andExpect(model().attributeExists("category"))
+                .andExpect(model().attribute("category", testCategory));
+    }
+    @Test
+    public void categoriesEndPointShouldDisplayAllCategories() throws Exception {
+        Category testCategory = new Category("Test");
+
+        List<Category> categoryList = Collections.singletonList(testCategory);
+
+        when(mockStorage.getAll()).thenReturn(categoryList);
+
+        mockMvc.perform(get("/category/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("category"))
+                .andExpect(model().attributeExists("categories"))
+                .andExpect(model().attribute("categories", categoryList));
+    }
+
+
+
 }
