8aacdd43790375366e3ca9c5d2c28b28322f9947
Noah Doughty <noahdoughty@gmail.com>
2020-02-22 21:52:31 -0500

    Tag Pojo
    Created Tag Pojo

--- src/main/java/org/wcci/blog/models/Tag.java
+++ src/main/java/org/wcci/blog/models/Tag.java
@@ -1,4 +1,43 @@
 package org.wcci.blog.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Collection;
+
+@Entity
 public class Tag {
+
+    @Id
+    @GeneratedValue
+    private Long tagId;
+    private String tagLabel;
+
+    @ManyToMany(mappedBy = "tags")
+    private Collection<Post> posts;
+
+    public Tag() {}
+
+    public Tag(String tagLabel) {
+        this.tagLabel = tagLabel;
+    }
+    public Collection<Post> getPosts() {
+        return posts;
+    }
+    public Long getTagId() {
+        return tagId;
+    }
+    public String getTagLabel() {
+        return tagLabel;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "tagId=" + tagId +
+                ", tagLabel='" + tagLabel + '\'' +
+                ", posts=" + posts +
+                '}';
+    }
 }
