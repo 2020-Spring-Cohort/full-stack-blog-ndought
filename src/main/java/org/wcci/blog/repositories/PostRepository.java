ddbf3d83ff53ae9130928a3f0b8475b630f3d7bd
Noah Doughty <noahdoughty@gmail.com>
2020-02-22 19:54:10 -0500

    Project Re-Start
    Restarted Project

--- src/main/java/org/wcci/blog/repositories/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package org.wcci.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.wcci.blog.entities.Post;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post,Long> {
-}
