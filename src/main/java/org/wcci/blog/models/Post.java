cf66fcc7cf2670d1426abc0633969ea23994564e
Noah Doughty <noahdoughty@gmail.com>
2020-02-22 21:43:36 -0500

    Post POJO
    Created Post POJO

--- src/main/java/org/wcci/blog/models/Post.java
+++ src/main/java/org/wcci/blog/models/Post.java
@@ -1,4 +1,74 @@
 package org.wcci.blog.models;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private LocalDateTime time;
+
+    @Lob
+    private String body;
+
+    @ManyToMany
+    private Collection<Author> authors;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToMany
+    private List<Tag> tags;
+
+
+    public Post() {
+    }
+    public Post(String title, String body, String time, Category category, Author author, Tag ...tags) {
+        this.title = title;
+        this.body = body;
+        this.category = category;
+        this.time = LocalDateTime.now();
+        this.authors = Arrays.asList(author);
+        this.tags = Arrays.asList(tags);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public Collection<Author> getAuthors() {
+        return authors;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+    public void addTagToTags(Tag tag) {
+        ArrayList<Tag> tags = new ArrayList<Tag>(this.getTags());
+        tags.add(tag);
+        this.tags = tags;
+    }
+    public Long getId() {
+        return id;
+    }
 }
