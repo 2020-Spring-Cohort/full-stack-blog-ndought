77ec00668f06870d4f3512ddfc24ce1f9025bd66
Noah Doughty <noahdoughty@gmail.com>
2020-02-28 14:05:42 -0500

    JpaWiringTest
    All test passing

--- src/test/java/org/wcci/blog/integrationTest/JpaWiringTest.java
+++ src/test/java/org/wcci/blog/integrationTest/JpaWiringTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.DirtiesContext;
 import org.wcci.blog.models.Category;
 import org.wcci.blog.models.Post;
+import org.wcci.blog.models.Tag;
 import org.wcci.blog.storage.repositories.CategoryRepository;
 import org.wcci.blog.storage.repositories.PostRepository;
 import org.wcci.blog.storage.repositories.TagRepository;
@@ -45,5 +46,40 @@ public class JpaWiringTest {
         Post retrievedPost = postRepo.findById(testPost.getId()).get();
 
         assertThat(retrievedCategory.getPosts()).contains(testPost);
+    }
+
+    @Test
+    public void tagsShouldBeAbleToHaveMultiplePosts() {
+        Category testCategory = new Category("sports");
+        Post testPost1 = new Post("name", "title", "content");
+        Post testPost2 = new Post("name", "title", "content");
+        Post testPost3 = new Post("name", "title", "content");
+
+        Tag testTag1 = new Tag("happy", testPost1, testPost2);
+        Tag testTag2 = new Tag("awesome", testPost1, testPost3);
+
+        categoryRepo.save(testCategory);
+
+        postRepo.save(testPost1);
+        postRepo.save(testPost2);
+        postRepo.save(testPost3);
+
+        tagRepo.save(testTag1);
+        tagRepo.save(testTag2);
+
+        testPost1.getTags().add(testTag1);
+        testPost1.getTags().add(testTag2);
+        testPost2.getTags().add(testTag1);
+        testPost3.getTags().add(testTag2);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Post retrievedPost = postRepo.findById(testPost1.getId()).get();
+        Tag retrievedTag1 = tagRepo.findById(testTag1.getId()).get();
+        Tag retrievedTag2 = tagRepo.findById(testTag2.getId()).get();
+        assertThat(retrievedPost.getTags()).contains(testTag1, testTag2);
+        assertThat(retrievedTag1.getPosts()).contains(testPost1, testPost2);
+        assertThat(retrievedTag2.getPosts()).contains(testPost1, testPost3);
     }
 }
