159b8aacc94f4761a7d3fd9bafaf576c6d1b29e5
Noah Doughty <noahdoughty@gmail.com>
2020-02-27 20:28:23 -0500

    CategoryControllerTest
    First two test in CategoryControllerTest pass

--- src/test/java/org/wcci/blog/controllerTest/CategoryControllerTest.java
+++ src/test/java/org/wcci/blog/controllerTest/CategoryControllerTest.java
@@ -2,14 +2,10 @@ package org.wcci.blog.controllerTest;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.stubbing.OngoingStubbing;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoProperties;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
-import org.wcci.blog.controllers.CategoryController;
+import org.wcci.blog.controllers.CategoriesController;
 import org.wcci.blog.models.Category;
 import org.wcci.blog.storage.CategoryStorage;
 
@@ -19,24 +15,23 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class CategoryControllerTest {
 
     private MockMvc mockMvc;
-    private CategoryController underTest;
+    private CategoriesController underTest;
     private CategoryStorage mockStorage;
     private Model mockModel;
 
-    @Autowired
-    private CategoryStorage storage;
 
     @BeforeEach
     void setUp() {
         mockModel = mock(Model.class);
         mockStorage = mock(CategoryStorage.class);
-        underTest = new CategoryController(mockStorage);
+        underTest = new CategoriesController(mockStorage);
         mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
     }
     @Test
@@ -54,33 +49,5 @@ public class CategoryControllerTest {
         String viewName = underTest.displaySingleCategory("tech", mockModel);
         assertThat(viewName).isEqualTo("category");
     }
-    @Test
-    public void shouldGoToIndividualEndPoint() throws Exception {
-        Category testCategory = new Category("sports");
-        when(mockStorage.findCategoryByName("sports")).thenReturn(testCategory);
-
-        mockMvc.perform(get("/category/sports"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("category"))
-                .andExpect(model().attributeExists("category"))
-                .andExpect(model().attribute("category", testCategory));
-    }
-    @Test
-    public void categoriesEndPointShouldDisplayAllCategories() throws Exception {
-        Category testCategory = new Category("Test");
-
-        List<Category> categoryList = Collections.singletonList(testCategory);
-
-        when(mockStorage.getAll()).thenReturn(categoryList);
-
-        mockMvc.perform(get("/category/"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("category"))
-                .andExpect(model().attributeExists("categories"))
-                .andExpect(model().attribute("categories", categoryList));
-    }
-
-
 
 }
