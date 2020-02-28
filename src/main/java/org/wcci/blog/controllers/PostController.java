2e28401aabba17977b34e4b5451116eb4718167d
Noah Doughty <noahdoughty@gmail.com>
2020-02-27 21:48:37 -0500

    PostControllerTest
    Wrote first test which failed, Created method and mapping for display post- test still failed, created second method and mapping for post id- test passed

--- src/main/java/org/wcci/blog/controllers/PostController.java
+++ src/main/java/org/wcci/blog/controllers/PostController.java
@@ -4,7 +4,10 @@ package org.wcci.blog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.wcci.blog.models.Post;
+import org.wcci.blog.storage.CategoryStorage;
 import org.wcci.blog.storage.PostStorage;
 import org.wcci.blog.storage.TagStorage;
 
@@ -12,22 +15,29 @@ import org.wcci.blog.storage.TagStorage;
 @RequestMapping("blog")
 public class PostController {
 
+    private CategoryStorage categoryStorage;
     private PostStorage storage;
+    private TagStorage tagStorage;
 
-
-    public PostController(PostStorage storage) {
+    public PostController(CategoryStorage categoryStorage, PostStorage storage, TagStorage tagStorage) {
+        this.categoryStorage = categoryStorage;
         this.storage = storage;
-
+        this.tagStorage = tagStorage;
     }
-    @RequestMapping
-    public String post(Model model) {
-        model.addAttribute("title", "My Blog");
+
+//    Root Path
+    @GetMapping
+    public String displayPost(Model model) {
+        model.addAttribute("posts", storage.getAll());
+        model.addAttribute("categories", categoryStorage.getAll());
+        return "post";
+    }
+    @GetMapping("/{id}")
+    public String displayPost(@PathVariable long id, Model model) {
+        Post retrievedPost = storage.findPostById(id);
+        model.addAttribute("post", retrievedPost);
+        model.addAttribute("tags", tagStorage.getAll());
         return "post";
     }
 
-//    @GetMapping
-//    public String displayPosts(Model model) {
-//        model.addAttribute("post", storage.getAll());
-//        return "post";
-//    }
 }
--- src/test/java/org/wcci/blog/controllerTest/PostControllerTest.java
+++ src/test/java/org/wcci/blog/controllerTest/PostControllerTest.java
@@ -1,4 +1,49 @@
 package org.wcci.blog.controllerTest;
 
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import org.wcci.blog.controllers.PostController;
+import org.wcci.blog.models.Category;
+import org.wcci.blog.models.Post;
+import org.wcci.blog.storage.CategoryStorage;
+import org.wcci.blog.storage.PostStorage;
+import org.wcci.blog.storage.TagStorage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class PostControllerTest {
+
+    private PostController underTest;
+    private Model model;
+    private PostStorage mockStorage;
+    private CategoryStorage mockCategoryStorage;
+    private TagStorage mockTagStorage;
+    private Post testPost;
+    private MockMvc mockMvc;
+    private Category testCategory;
+
+    @BeforeEach
+    void setUp() {
+        mockStorage = mock(PostStorage.class);
+        mockCategoryStorage = mock(CategoryStorage.class);
+        mockTagStorage = mock(TagStorage.class);
+        underTest = new PostController(mockCategoryStorage, mockStorage, mockTagStorage);
+        mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
+        model = mock(Model.class);
+        testCategory = new Category("tech");
+        testPost = new Post("bill", "hello", "test");
+        when(mockCategoryStorage.findCategoryByName("tech")).thenReturn(testCategory);
+        when(mockStorage.findPostById(1L)).thenReturn(testPost);
+    }
+    @Test
+    public void displayPostReturnsPostTemplate() {
+        String result = underTest.displayPost(1L, model);
+        assertThat(result).isEqualTo("post");
+    }
 }
