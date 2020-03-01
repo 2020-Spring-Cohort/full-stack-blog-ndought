801344892a26837a07202088be81ea1f68cdd86e
Noah Doughty <noahdoughty@gmail.com>
2020-03-01 16:52:21 -0500

    WebLayerTest
    1st test failing in red, changed post to posts now failing in yellow, need more investigation

--- src/test/java/org/wcci/blog/integrationTest/WebLayerTest.java
+++ src/test/java/org/wcci/blog/integrationTest/WebLayerTest.java
@@ -1,4 +1,45 @@
 package org.wcci.blog.integrationTest;
 
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.wcci.blog.storage.AuthorStorage;
+import org.wcci.blog.storage.CategoryStorage;
+import org.wcci.blog.storage.PostStorage;
+import org.wcci.blog.storage.TagStorage;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
 public class WebLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    AuthorStorage authorStorage;
+    @MockBean
+    PostStorage mockStorage;
+    @MockBean
+    CategoryStorage categoryStorage;
+    @MockBean
+    TagStorage tagStorage;
+
+    @Test
+    public void postsShouldBeOkAndReturnThePostViewWithPostsModelAttribute() throws Exception {
+        mockMvc.perform(get("/posts"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post"))
+                .andExpect(model().attributeExists("post"));
+
+    }
 }
