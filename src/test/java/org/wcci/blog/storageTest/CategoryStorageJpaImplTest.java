63efa2ab68891b7edb7892cbb3636c3a89eecc03
Noah Doughty <noahdoughty@gmail.com>
2020-03-01 17:14:18 -0500

    CategoryStorageJpaImplTest
    1st test store Cat- test passed

--- src/test/java/org/wcci/blog/storageTest/CategoryStorageJpaImplTest.java
+++ src/test/java/org/wcci/blog/storageTest/CategoryStorageJpaImplTest.java
@@ -1,4 +1,27 @@
 package org.wcci.blog.storageTest;
 
+import org.junit.jupiter.api.Test;
+import org.wcci.blog.models.Category;
+import org.wcci.blog.storage.CategoryStorage;
+import org.wcci.blog.storage.CategoryStorageJpaImpl;
+import org.wcci.blog.storage.repositories.CategoryRepository;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
 public class CategoryStorageJpaImplTest {
+
+    @Test
+    public void shouldStoreCategory() {
+        CategoryRepository categoryRepository = mock(CategoryRepository.class);
+        CategoryStorage storage = new CategoryStorageJpaImpl(categoryRepository);
+        Category testCategory = new Category("tech");
+        when(categoryRepository.findAll()).thenReturn(Collections.singletonList(testCategory));
+        storage.store(testCategory);
+        verify(categoryRepository).save(testCategory);
+        assertThat(storage.getAll()).contains(testCategory);
+    }
 }
